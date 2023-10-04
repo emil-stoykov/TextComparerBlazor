738352667fe0c9f2ac98ec833b96c10c197674b0
emil stoykov <emilstojkov087@gmail.com>
2023-10-04 16:20:51 +0300

    Created two classes "Result" and "ResultService"

---
+++ TextComparer/Data/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace TextComparer.Data
+{
+    public class Result
+    {
+        public Result() {
+            LinesWithError = new List<int>();
+        }
+
+        public int TotalLines { get; set; }
+
+        public ICollection<int> LinesWithError { get; set; }
+
+        public int ErrorLines => LinesWithError.Count;
+
+        public bool IsErrorless { get; set; } = true;
+    }
+}
---
+++ TextComparer/Data/ResultService.cs
@@ -0,0 +1,33 @@
+﻿namespace TextComparer.Data
+{
+    public class ResultService
+    {
+        public ResultService() {
+            Result = new Result();
+        }
+        public Result Result { get; set; }
+
+
+        /// <summary>
+        /// An asynchronous method that compares two texts line by line. Returns a "Result" object.
+        /// </summary>
+        /// <param name="text1"></param>
+        /// <param name="text2"></param>
+        /// <returns></returns>
+        public async Task<Result> CheckLines(string[] text1, string[] text2)
+        {
+            Result.TotalLines = Math.Min(text1.Length, text2.Length);
+
+            for (int line = 0; line < Result.TotalLines; line++)
+            {
+                if (!text1[line].Equals(text2[line]))
+                {
+                    Result.IsErrorless = false;
+                    Result.LinesWithError.Add(line);
+                } 
+            }
+
+            return await Task.FromResult(Result);
+        }
+    }
+}
